0f1915af0dcaa243121daf4e90115a67223e7a42
baetzmr <81546284+baetzmr@users.noreply.github.com>
2022-11-22 19:13:46 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 
 enum class Mode { scan, enroll, wificonfig, maintenance };
 
-const char* VersionInfo = "0.54_Markus";
+const char* VersionInfo = "0.54";
 
 // ===================================================================================================================
 // Caution: below are not the credentials for connecting to your home network, they are for the Access Point mode!!!
